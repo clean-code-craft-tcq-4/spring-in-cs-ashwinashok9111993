870dbb0748ccdac07f14edbddef6e10978394c20
ashwin <ashwinashok9111993@gmail.com>
2022-05-16 11:41:01 +0530

    Update Statistics.cs
    Test for average

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,17 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Statistics
 {
     public class Stats
     {
-        
+        public float average(List<float> numbers)
+        {
+            float avg = 0;
+            int count = 0;
+            foreach(float num in numbers)
+            {
+                avg = avg + num;
+                count++;
+            }
+
+            return avg/count;
+        }
     }
     public class StatsComputer
     {
         public Stats CalculateStatistics(List<float> numbers) {
             //Implement statistics here
-            
+
+            Stats stats = new Stats();
+            return stats;
         }
     }
 }
