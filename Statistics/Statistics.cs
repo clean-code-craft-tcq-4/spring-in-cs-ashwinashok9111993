0329c6a2cd07bc9f6aa86facfb6f8c85fe57caed
ashwin <ashwinashok9111993@gmail.com>
2022-05-16 11:16:34 +0530

    Update Statistics.cs
    initial test

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 
 namespace Statistics
 {
+    public class Stats()
+    {
+        
+    }
     public class StatsComputer
     {
         public Stats CalculateStatistics(List<float> numbers) {
             //Implement statistics here
+            
         }
     }
 }
